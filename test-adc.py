2d392e5442cd4b8a74d0b3653059ecd856276b58
ypsitau <ypsitau@nifty.com>
2024-03-26 15:50:56 +0900

    implementing adc test

--- test-adc.py
+++ test-adc.py
@@ -1,2 +1,78 @@
 #!/usr/bin/env python
-from ADCDevice import *
+import time
+import smbus
+
+class ADCDevice(object):
+    def __init__(self):
+        self.cmd = 0
+        self.address = 0
+        self.bus=smbus.SMBus(1)
+        # print("ADCDevice init")
+        
+    def detectI2C(self,addr):
+        try:
+            self.bus.write_byte(addr,0)
+            print("Found device in address 0x%x"%(addr))
+            return True
+        except:
+            print("Not found device in address 0x%x"%(addr))
+            return False
+            
+    def close(self):
+        self.bus.close()
+        
+class PCF8591(ADCDevice):
+    def __init__(self):
+        super(PCF8591, self).__init__()
+        self.cmd = 0x40     # The default command for PCF8591 is 0x40.
+        self.address = 0x48 # 0x48 is the default i2c address for PCF8591 Module.
+        
+    def analogRead(self, chn): # PCF8591 has 4 ADC input pins, chn:0,1,2,3
+        value = self.bus.read_byte_data(self.address, self.cmd+chn)
+        value = self.bus.read_byte_data(self.address, self.cmd+chn)
+        return value
+    
+    def analogWrite(self,value): # write DAC value
+        self.bus.write_byte_data(address,cmd,value)	
+
+class ADS7830(ADCDevice):
+    def __init__(self):
+        super(ADS7830, self).__init__()
+        self.cmd = 0x84
+        self.address = 0x4b # 0x4b is the default i2c address for ADS7830 Module.   
+        
+    def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn:0,1,2,3,4,5,6,7
+        value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
+        return value
+
+adc = ADCDevice() # Define an ADCDevice class object
+
+def setup():
+    global adc
+    if(adc.detectI2C(0x48)): # Detect the pcf8591.
+        adc = PCF8591()
+    elif(adc.detectI2C(0x4b)): # Detect the ads7830
+        adc = ADS7830()
+    else:
+        print("No correct I2C address found, \n"
+        "Please use command 'i2cdetect -y 1' to check the I2C address! \n"
+        "Program Exit. \n");
+        exit(-1)
+        
+def loop():
+    while True:
+        value = adc.analogRead(0)    # read the ADC value of channel 0
+        voltage = value / 255.0 * 3.3  # calculate the voltage value
+        print ('ADC Value : %d, Voltage : %.2f'%(value,voltage))
+        time.sleep(0.1)
+
+def destroy():
+    adc.close()
+    
+if __name__ == '__main__':   # Program entrance
+    print ('Program is starting ... ')
+    try:
+        setup()
+        loop()
+    except KeyboardInterrupt: # Press ctrl-c to end the program.
+        destroy()
